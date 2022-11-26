4fa0775a351c18276258f6da310ff5e653e3053d
Eu <thaseugene@gmail.com>
2022-11-26 21:07:45 +0300

    added working with DB

--- src/main/java/com/example/testchat/service/ChatService.java
+++ src/main/java/com/example/testchat/service/ChatService.java
@@ -17,34 +17,6 @@ public class ChatService {
         this.eventBus = new ComponentEventBus(new Div());
     }
 
-//    public void addRecord(String user, String message) {
-//        messagesStorage.save(new ChatMessage(user, LocalDateTime.now(), message));
-//        eventBus.fireEvent(new ChatEvent());
-//    }
-//
-//    public long size() {
-//        return messagesStorage.count();
-//    }
-//
-//    public void addRecordJoined(String user) {
-//        messagesStorage.save(new ChatMessage("",LocalDateTime.now(), user));
-//        eventBus.fireEvent(new ChatEvent());
-//    }
-
-//    @Getter
-//    @AllArgsConstructor
-//    public static class ChatMessage {
-//        private String name;
-//        private LocalDateTime localDateTime;
-//        private String message;
-//
-//    }
-
-    //    public static class ChatEvent extends ComponentEvent<Div> {
-//        public ChatEvent() {
-//            super(new Div(), false);
-//        }
-//    }
     public Registration attachListener(ComponentEventListener<ChatEvent> eventListener) {
         return eventBus.addListener(ChatEvent.class, eventListener);
     }
